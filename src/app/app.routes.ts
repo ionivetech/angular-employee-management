d32e2e24564710f5d871ab46d9f99e3549431725
ionivetech <ionivetech@gmail.com>
2024-02-17 10:12:48 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+// Components
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './dashboard/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'user',
+        component: UserComponent,
+      },
+    ],
+  },
+];
---
+++ src/app/dashboard/user/user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss',
+})
+export class UserComponent {}
